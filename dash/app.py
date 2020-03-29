04c94aa27e06607686490ff1d4f71f2a2955b059
AndreasF-O <andreasfo@gmail.com>
2020-03-29 12:54:49 +0200

    Added dash functionality. Please take a look bro

--- dash/app.py
+++ dash/app.py
@@ -12,16 +12,7 @@ app = dash.Dash()
 
 app.layout = layout.generate_layout()
 
-<<<<<<< HEAD
 
-@app.callback(Output('total_deaths', 'figure'),
-              [Input('data', 'children'),
-               Input('country_selection1', 'value')])
-def update_total_deaths(json_obj, countries):
-    if not json_obj or not countries:
-        return go.Figure()
-    df_deaths = pd.read_json(json.loads(json_obj)['deaths'])
-=======
 titles = {'df_deaths': 'COVID19 Total Nordic Deaths, starting March 10 2020',
           'df_deaths_per_mn': 'COVID19 Total Nordic Deaths per Mn inhabitants, starting March 10 2020',
           'df_deaths_1': 'COVID19 Total Nordic Deaths, daily data since first death',
@@ -37,79 +28,16 @@ def update_total_deaths(json_obj, countries, which_plot):
         return go.Figure()
     df_deaths = pd.read_json(json.loads(json_obj)[which_plot])
     title = titles[which_plot]
->>>>>>> Added dash functionality. Please take a look bro
     fig = go.Figure()
     for col in countries:
         fig.add_trace(go.Scatter(x=df_deaths.index,
                                  y=df_deaths[col], name=col))
-<<<<<<< HEAD
-    fig.update_layout(title_text='COVID19 Total Nordic Deaths, starting March 10 2020', hovermode='x',
-                      xaxis_rangeslider_visible=True)
-
-    return fig
-
-
-@app.callback(Output('Deaths/mn', 'figure'),
-              [Input('data', 'children'),
-               Input('country_selection2', 'value')])
-def update_total_deaths(json_obj, countries):
-    if not json_obj or not countries:
-        return go.Figure()
-    df_deaths_per_mn = pd.read_json(json.loads(json_obj)['deaths_per_mn'])
-
-    fig = go.Figure()
-    for col in countries:
-        fig.add_trace(go.Scatter(x=df_deaths_per_mn.index,
-                                 y=df_deaths_per_mn[col], name=col))
-
-    fig.update_layout(title_text='COVID19 Total Nordic Deaths per Mn inhabitants, starting March 10 2020', hovermode='x',
-=======
     fig.update_layout(title_text=title, hovermode='x',
->>>>>>> Added dash functionality. Please take a look bro
                       xaxis_rangeslider_visible=True)
 
     return fig
 
 
-<<<<<<< HEAD
-@app.callback(Output('daily_deaths', 'figure'),
-              [Input('data', 'children'),
-               Input('country_selection3', 'value')])
-def update_total_deaths(json_obj, countries):
-    if not json_obj or not countries:
-        return go.Figure()
-    df_deaths_1 = pd.read_json(json.loads(json_obj)['deaths_1'])
-
-    fig = go.Figure()
-    for col in countries:
-        fig.add_trace(go.Scatter(x=df_deaths_1.index,
-                                 y=df_deaths_1[col], name=col))
-
-    fig.update_layout(template='plotly_white', title_text='COVID19 Total Nordic Deaths, daily data since first death', hovermode='x',
-                      xaxis_rangeslider_visible=True)
-    return fig
-
-
-@app.callback(Output('daily_deaths/mn', 'figure'),
-              [Input('data', 'children'),
-               Input('country_selection4', 'value')])
-def update_total_deaths(json_obj, countries):
-    if not json_obj or not countries:
-        return go.Figure()
-    df_deaths_per_mn_1 = pd.read_json(json.loads(json_obj)['deaths_per_mn_1'])
-
-    fig = go.Figure()
-    for col in countries:
-        fig.add_trace(go.Scatter(x=df_deaths_per_mn_1.index,
-                                 y=df_deaths_per_mn_1[col], name=col))
-
-    fig.update_layout(template='plotly_white', title_text='COVID19 Total Nordic Deaths per Mn inhabitants, daily data since first death', hovermode='x',
-                      xaxis_rangeslider_visible=True)
-    return fig
-
-
-=======
->>>>>>> Added dash functionality. Please take a look bro
 if __name__ == "__main__":
     app.run_server(debug=True)
     pass
